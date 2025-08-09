73ef7bfa40d3cb60791e4d7e9d4fcd89ead332a4
chahe-dridi <chaher.dridi.100@gmail.com>
2025-08-09 15:01:35 +0100

    pipline for backend is all set works fine

--- Prelevements_par_caisse/Controllers/WeatherForecastController.cs
+++ Prelevements_par_caisse/Controllers/WeatherForecastController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.VisualBasic;
 
 namespace Prelevements_par_caisse.Controllers
 {
@@ -7,9 +6,6 @@ namespace Prelevements_par_caisse.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        VariantType dateofbirth = new VariantType();
-        int xd = 0;
-
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
