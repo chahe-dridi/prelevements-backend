e01864fe2da7fa314a152d5c82cc9bcfe8b09cbc
chahe-dridi <chaher.dridi.100@gmail.com>
2025-08-11 17:46:00 +0100

    working on with register/login

---
+++ Prelevements_par_caisse/DTOs/AuthRequests.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Prelevements_par_caisse.DTOs
+{
+    public class RegisterRequest
+    {
+        [Required] public string Nom { get; set; }
+        [Required] public string Prenom { get; set; }
+        [Required, EmailAddress] public string Email { get; set; }
+        [Required] public string Password { get; set; }
+    }
+
+    public class LoginRequest
+    {
+        [Required, EmailAddress] public string Email { get; set; }
+        [Required] public string Password { get; set; }
+    }
+}
